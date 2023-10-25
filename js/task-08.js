4d0899566acf44f7e66a101750d20d8a0c4eaf02
Vorobole <vorobyov1903@gmail.com>
2023-10-25 12:47:02 +0300

    Task08-update

--- js/task-08.js
+++ js/task-08.js
@@ -2,15 +2,18 @@ const loginForm = document.querySelector(".login-form");
 
 loginForm.addEventListener("submit", function (e) {
   e.preventDefault();
-  const formData = new FormData(loginForm);
-  const formObject = {};
-  formData.forEach((value, key) => {
-    formObject[key] = value;
-  });
 
-  if (!formObject.email || !formObject.password) {
+  const emailInput = loginForm.querySelector('input[name="email"]');
+  const passwordInput = loginForm.querySelector('input[name="password"]');
+
+  if (!emailInput.value || !passwordInput.value) {
     alert("All fields must be filled out!");
   } else {
+    const formObject = {
+      email: emailInput.value,
+      password: passwordInput.value,
+    };
+
     console.log(formObject);
     loginForm.reset();
   }
