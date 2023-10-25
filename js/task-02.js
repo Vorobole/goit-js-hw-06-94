f8598fcbb9302ca8b422cb41000baf145363166a
Vorobole <vorobyov1903@gmail.com>
2023-10-25 12:40:31 +0300

    Task02-update

--- js/task-02.js
+++ js/task-02.js
@@ -2,16 +2,19 @@ const ingredients = [
   "Potatoes",
   "Mushrooms",
   "Garlic",
-  "Tomatos",
+  "Tomatoes",
   "Herbs",
   "Condiments",
 ];
 
 const ingredientsList = document.querySelector("#ingredients");
+const listItems = [];
 
 ingredients.forEach((ingredient) => {
   const listItem = document.createElement("li");
   listItem.textContent = ingredient;
   listItem.classList.add("item");
-  ingredientsList.appendChild(listItem);
+  listItems.push(listItem);
 });
+
+ingredientsList.append(...listItems);
