c11dadf6c02ddb54f57e3bf2e66c4168772a9429
Vorobole <vorobyov1903@gmail.com>
2023-10-25 12:43:00 +0300

    Task05-update

--- js/task-05.js
+++ js/task-05.js
@@ -2,9 +2,6 @@ const nameInput = document.querySelector("#name-input");
 const nameOutput = document.querySelector("#name-output");
 
 nameInput.addEventListener("input", () => {
-  if (nameInput.value) {
-    nameOutput.textContent = nameInput.value;
-  } else {
-    nameOutput.textContent = "Anonymous";
-  }
+  const trimmedValue = nameInput.value.trim();
+  nameOutput.textContent = trimmedValue || "Anonymous";
 });
